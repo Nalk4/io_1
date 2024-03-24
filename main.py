5be6b03ef995c76840454c405dab6e258aaa5e6d
Natalia Dziwisz <nataliadziwisz13@gmail.com>
2024-03-24 12:18:40 +0100

    trojkat i trapez

--- main.py
+++ main.py
@@ -6,3 +6,16 @@ bok_a = 5
 bok_b = 10
 pole_prstk = (bok_a)*(bok_b)
 print (pole_prstk)
+
+bok_a = 5
+bok_b = 15
+h = 3
+pole_trjk = ((bok_b)*h)/2
+print (pole_trjk)
+
+bok_a = 3
+bok_b = 8
+h = 13
+pole_trap = ((bok_a)+(bok_b))*h/2
+print (pole_trap)
+
